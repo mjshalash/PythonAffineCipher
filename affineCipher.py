1203894fc806169311d7426bafc6ac1b180b91f4
Malik Shalash <mjshal03@louisville.edu>
2019-02-03 10:45:53 -0500

    Base program working with hardcoded message
    Next step is to implement functionality for reading in a text file and exncrypting/decrypting this

--- affineCipher.py
+++ affineCipher.py
@@ -2,16 +2,20 @@
 # Affine Cipher
 # 1/31/2019
 
-import sys, pyperclip, cryptomath, random
+import sys, cryptomath, random
 
 # What is allowed to be decrypted
 SYMBOLS = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\] ^_`abcdefghijklmnopqrstuvwxyz{|}~"""
 
 # Main Function
 def main():
-    myMessage = ""
-    myKey = 0
-    myMode = 'e'
+    myMessage = "IPv[f5 =$-Rvnf={svs-j-RV-v$fvB-v[){{-sv1N$-{{1^-N$v1Fv1$v[f={svs-[-1V-v)vw=5)Nv1N$fvB-{1-V1N^v$w)$v1$vn)jvw=5)NUIv<P{)NvK=R1N^"
+    myKey = 2487
+    myMode = 'd'
+
+    # Print statements for logging purposes
+    print('Key: %s' % (myKey))
+    print('Size of Symbols: %d' %(len(SYMBOLS)))
 
     # Determines whether we are encrypting a message or decrypting a message
     if myMode == 'e':
@@ -24,8 +28,6 @@ def main():
     print('Key: %s' % (myKey))
     print('%sed text:' % (myMode.title()))
     print(answer)
-    pyperclip.copy(answer)
-    print('Full %sed text copied to clipboard.' % (myMode))
 
 # Get Key Parts
 def getKeyParts(key):
@@ -34,24 +36,33 @@ def getKeyParts(key):
     return (a, b)              # Return tuple of both keys
 
 def checkKeys(keyA, keyB, mode):
+    if keyA == 1 and mode == 'encrypt':
+        sys.exit('The affine cipher becomes incredibly weak when key A is set to 1. Choose a different key.')
+    if keyB == 0 and mode == 'encrypt':
+        sys.exit('The affine cipher becomes incredibly weak when key B is set to 0. Choose a different key.')
     if keyA < 0 or keyB < 0 or keyB > len(SYMBOLS) - 1:
         sys.exit('Key A must be greater than 0 and Key B must be between 0 and %s.' % (len(SYMBOLS) - 1))
     if cryptomath.gcd(keyA, len(SYMBOLS)) != 1:
-        sys.exit('Key A (%s) and the symbol set size (%s) are not relatively prime. Choose a different key.' % (keyA, len(SYMBOLS)))
-    
+        sys.exit('Key A (%s) and the symbol set size (%s) are not relatively prime. Choose a different key.' % (keyA, len(SYMBOLS)))  
+
+      
 def encrypt(key, message):
     a, b = getKeyParts(key)  # Establish both key and key b by returning a tuple from the function
+
+    print('Key A is %d', a)
+    print('Key B is %d', b)
+
     checkKeys(a, b, 'e')     # Validate that keys are correct
     cipherText = ''
     for symbol in message:
             if symbol in SYMBOLS:
                 # Encrypt Specific Symbol
                 symIndex = SYMBOLS.find(symbol)
-                ciphertext += SYMBOLS[(symIndex * a + b) % len(SYMBOLS)]
+                cipherText += SYMBOLS[(symIndex * a + b) % len(SYMBOLS)]
             else:
                 # Append the symbol to our current solution
-                ciphertext += symbol
-    return ciphertext
+                cipherText += symbol
+    return cipherText
 
 def decrypt(key, message):
     keyA, keyB = getKeyParts(key)
