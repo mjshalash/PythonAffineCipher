bc8d58674464660136af8c70ab3da37b663f50bb
Malik Shalash <mjshal03@louisville.edu>
2019-02-07 21:06:53 -0500

    Added grabbing the ascii values of two most common

--- attack.py
+++ attack.py
@@ -2,10 +2,32 @@
 # Affine Cipher Attack
 # 1/31/2019
 
+# import statements
+import sys, os, collections
+
 # Read in encrypted file
 def main():
-    # Get frequency count of each letter in the 
+    
+    # Define file to be read in
+    file = open("testFiles/test.txt", "r")
+    myMessage = file.read()
 
+    # Assume that " " and "e" are the two most common charachters in whatever needs to be decrypted
+    firstCount = collections.Counter(myMessage).most_common(2)[0]   # Assume this charachter is ' '
+    secondCount = collections.Counter(myMessage).most_common(2)[1]   # Assume this charachter is e
+
+    # Get the charachters
+    firstComChar = getCommon(firstCount)
+    secondComChar = getCommon(secondCount)
+
+    # Find the ascii values for charachters
+    print(ord(firstComChar))
+    print(ord(secondComChar))
+
+def getCommon(list):
+    # Return just the charachter
+    return list[0]
+    
 
 
 
