ac6b562faa205439240fc7cad7adbe246f87002a
Malik Shalash <mjshal03@louisville.edu>
2019-02-10 20:24:28 -0500

    More organizing

--- attack.py
+++ attack.py
@@ -23,23 +23,17 @@ def attackCipher(ciphertext):
     a = (115*(firstAscii - secondAscii)) % (256) # 115 is inverse of 101 - 32 = 69
     b = (firstAscii - (a)*(asciiSpace)) % (256)
 
-    print("This is a: ", (a % 256))
-    print("This is b: ", (b % 256))
-
     mainAffine.checkKeys(a, b, 'd')
 
     plaintext = ''
     modInverseOfKeyA = mainAffine.findModInverse(a, 256)
 
-    print("Mod Inverse of A is: ", modInverseOfKeyA)
-
     for symbol in ciphertext:
         symIndex = ord(symbol)
         plaintext += chr((symIndex - b) * modInverseOfKeyA % 256)
     
-    print(plaintext)
+    print("Attack result: ", plaintext)
 
 # Get only the charachter from the list
 def getCommon(list):
-    
     return list[0]
